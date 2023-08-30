dd92f58e8d73e1fa48d5f0a7f341c390927b8528
Cristian Medina <cristian.medina@usal.edu.ar>
2023-08-30 13:29:31 -0400

    Login wrong y vacio más variables

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -3,7 +3,7 @@ import { variables } from '../variables/variables';
 
 test('ingresar a la página', async ({ page }) => {
   try {
-    await page.goto(variables.Payrolltest);
+    await page.goto(variables.payrollTest);
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Payroll/);
@@ -14,7 +14,7 @@ test('ingresar a la página', async ({ page }) => {
 });
 
 test('login vacio', async ({ page }) => {
-  await page.goto(variables.Payrolltest);
+  await page.goto(variables.payrollTest);
   await page.getByPlaceholder('name@example.com').click();
   await page.getByPlaceholder('Password').click();
   await page.locator('div').filter({ hasText: /^Login$/ }).click();
@@ -22,4 +22,15 @@ test('login vacio', async ({ page }) => {
   expect(errorMessage).toContain('Please provide a valid email address.');
   const errorMessage2 = await page.textContent(':text("Required")');
   expect(errorMessage2).toContain('Required');
+});
+
+test('login wrong', async({ page }) => {
+  await page.goto(variables.payrollTest);
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(variables.emailWrong);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(variables.passwordWrong);
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  const errorMessage = await page.textContent(':text("Login failed")');
+  expect(errorMessage).toContain('Login failed');
 });
--- variables/variables.ts
+++ variables/variables.ts
@@ -1,8 +1,14 @@
 export const variables = {
     
-    Payrolltest: 'https://payrollfront.azurewebsites.net/#/login'
-
-
-
+    payrollTest: 'https://payrollfront.azurewebsites.net/#/login',
+    emailAdmin: 'gerardoa@4waystech.com',
+    passwordAdmin: 'abcD#1234',
+    emailSindicatoAdmin: 'adminsindicato@4waystech.com',
+    passwordSindicato: 'Adminempleados1',
+    emaileEmpleadoAdmin:  'adminempleados@4waystech.com',
+    passwordEmpleado: 'Adminempleados1',
+    emailWrong: 'asd@asd.com',
+    passwordWrong: 'asd',
+    
 };
 
