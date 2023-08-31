ae420f86c937fe68d447d3c1f14078b2245a61a5
Cristian Medina <cristian.medina@usal.edu.ar>
2023-08-31 10:39:04 -0400

    Login completo y fix en variables

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -34,3 +34,68 @@ test('login wrong', async({ page }) => {
   const errorMessage = await page.textContent(':text("Login failed")');
   expect(errorMessage).toContain('Login failed');
 });
+
+test('login fail password', async ({ page }) => {
+  await page.goto(variables.payrollTest);
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(variables.emailAdmin);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(variables.passwordWrong);
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  const errorMessage = await page.textContent(':text("Login failed")');
+  expect(errorMessage).toContain('Login failed');
+
+
+})
+
+test('login ok administrator', async({ page }) => {
+  await page.goto(variables.payrollTest);
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(variables.emailAdmin);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(variables.passwordAdmin);
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  try{
+    await expect(page).toHaveURL('https://payrollfront.azurewebsites.net/#/');
+
+  } catch(error) {
+    console.error("Algo pasó con el usuario administrador, no pudo logearse correctamente o la URL no es la misma", error);
+    throw error;
+  }  
+  
+});
+
+test('login ok user sindicate', async({ page }) => {
+  await page.goto(variables.payrollTest);
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(variables.emailSindicatoAdmin);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(variables.passwordSindicato);
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  try{
+    await expect(page).toHaveURL('https://payrollfront.azurewebsites.net/#/');
+
+  } catch(error) {
+    console.error("Algo pasó con el usuario sindicato, no pudo logearse correctamente o la URL no es la misma", error);
+    throw error;
+  }  
+  
+});
+
+test('login ok user empleado', async({ page }) => {
+  await page.goto(variables.payrollTest);
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(variables.emaileEmpleadoAdmin);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(variables.passwordEmpleado);
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  await page.waitForURL('https://payrollfront.azurewebsites.net/#/');
+  try{
+    await expect(page).toHaveURL('https://payrollfront.azurewebsites.net/#/');
+
+  } catch(error) {
+    console.error("Algo pasó con el usuario empleado, no pudo logearse correctamente o la URL no es la misma", error);
+    throw error;
+  }  
+  
+});
--- variables/variables.ts
+++ variables/variables.ts
@@ -4,7 +4,7 @@ export const variables = {
     emailAdmin: 'gerardoa@4waystech.com',
     passwordAdmin: 'abcD#1234',
     emailSindicatoAdmin: 'adminsindicato@4waystech.com',
-    passwordSindicato: 'Adminempleados1',
+    passwordSindicato: 'Adminsindicato1',
     emaileEmpleadoAdmin:  'adminempleados@4waystech.com',
     passwordEmpleado: 'Adminempleados1',
     emailWrong: 'asd@asd.com',
