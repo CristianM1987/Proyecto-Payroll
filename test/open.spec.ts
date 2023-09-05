3e876abceadb32f836fbe32717b4c16222096cd9
Cristian Medina <cristian.medina@usal.edu.ar>
2023-09-05 09:12:18 -0400

    open payroll complete

--- test/open.spec.ts
+++ test/open.spec.ts
@@ -7,6 +7,7 @@ import { menuPrincipal, principalChild } from '../variables/menu';
 
 test('open payroll', async({ page }) => {
     await page.goto(variables.payrollTest);
+    await page.waitForTimeout(1000);
     await page.getByPlaceholder('name@example.com').click();
     await page.getByPlaceholder('name@example.com').fill(variables.emailAdmin);
     await page.getByPlaceholder('Password').click();
@@ -33,20 +34,24 @@ test('open payroll', async({ page }) => {
     await page.getByText('1', { exact: true }).click();
     await page.getByText('31').click();
     await page.getByRole('button', { name: 'Save' }).click();
+    await page.waitForTimeout(2000);
     await page.locator(menuPrincipal.openPayrollMenu).click();
+    await expect(page).toHaveURL('https://payrollfront.azurewebsites.net/#/open-payroll'); 
+    await page.waitForTimeout(2000);
     await page.getByLabel('Cycles').selectOption('3: 1');
-    await page.getByLabel('Periods').selectOption('1: 122');
-    const selector = 'option:has-text("1/01/25 - 31/01/25")';
-    //await page.selectOption(selector); ver de seguir aquí con el selector nuevo!
+    await page.getByLabel('Periods').selectOption({ label: '1/01/25 - 31/01/25'});
+    //const selector = ('option:has-text("1/01/25 - 31/01/25")');
     await page.getByLabel('Type Header').selectOption('3: 3');
     await page.getByRole('button', { name: 'Open' }).click();
     await page.getByRole('button', { name: 'Yes' }).click();
     await page.getByRole('button', { name: 'Ok' }).click();
-    await page.getByRole('button', { name: 'Main' }).click();
+    //await page.getByRole('button', { name: 'Main' }).click();
     await page.locator(menuPrincipal.mainMenu).click();
     await page.locator(principalChild.periodsMenu).click();
     await page.getByRole('link', { name: '' }).click();
     await page.getByRole('button', { name: 'Yes' }).click();
-
+    await page.waitForTimeout(2000);
+    const invisible = await page.getByLabel('1/01/25 - 31/01/25').isHidden();
+  
 
 });
