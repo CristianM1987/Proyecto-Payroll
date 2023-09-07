120be37c516e398614dc57276ffc9b15c09acc9e
Cristian Medina <cristian.medina@usal.edu.ar>
2023-09-07 09:14:32 -0400

    diario sindicato completo

--- test/journalSyndicate.spec.ts
+++ test/journalSyndicate.spec.ts
@@ -5,7 +5,7 @@ import { menuPrincipal, syndicateChild } from '../variables/menu';
 //el test va a abrir el modulo de trabajadores y va a revisar que un trabajador se encuentre listado
 
 
-test('hournal syndicate', async({ page }) => {
+test('journal syndicate', async({ page }) => {
     await page.goto(variables.payrollTest);
     await page.waitForTimeout(1000);
     await page.getByPlaceholder('name@example.com').click();
@@ -23,5 +23,43 @@ test('hournal syndicate', async({ page }) => {
 
     await page.locator(menuPrincipal.syndicateMenu).click();
     await page.locator(syndicateChild.syndicatePaymentJournalMenu).click();
-    
+    //seleccionar diario N° 10 y valida que haya entrado a ese diario
+    await page.locator('xpath=//*[@id="paymentJournalLine"]/tbody/tr[2]/td[8]/div/a/i').click();
+    const expectedText = '01_00000010';
+    await page.waitForSelector(`text="${expectedText}"`);
+    const empleadoElement = await page.locator(`text="${expectedText}"`);
+    const existeValor = await empleadoElement.isVisible();
+    await expect(existeValor).toBeTruthy();
+
+    //seleccionar archivos descargables
+    // Start waiting for download before clicking. Note no await.
+    const downloadPromise = page.waitForEvent('download');
+    await page.locator('xpath=//html/body/app-root/div/div/app-journal-syndicate-details/div/section/div/div/div[2]/div[2]/div/table/tbody/tr[2]/td[9]/button').click();
+    const download = await downloadPromise;
+    // Wait for the download process to complete
+    console.log(await download.path());
+    // Save downloaded file somewhere
+    await download.saveAs('abordo.pdf'); // genera el archivo de payroll del sindicato abordo
+    await page.locator('xpath=//html/body/app-root/div/div/app-journal-syndicate-details/div/section/div/div/div[2]/div[2]/div/table/tbody/tr[3]/td[9]/button').click();
+    // Wait for the download process to complete
+    console.log(await download.path());
+    // Save downloaded file somewhere
+    await download.saveAs('arrimo.pdf'); // genera el archivo de payroll del sindicato arrimo
+    await page.locator('xpath=//html/body/app-root/div/div/app-journal-syndicate-details/div/section/div/div/div[2]/div[2]/div/table/tbody/tr[4]/td[9]/button').click();
+    // Wait for the download process to complete
+    console.log(await download.path());
+    // Save downloaded file somewhere
+    await download.saveAs('unido.pdf'); // genera el archivo de payroll del sindicato unido
+
+    await page.locator('xpath=//html/body/app-root/div/div/app-journal-syndicate-details/div/div[2]/div/div[2]/button').click();
+    console.log(await download.path());
+    await download.saveAs('Payment_post.txt'); // Generar archivo de envío
+
+    await page.locator('xpath=//html/body/app-root/div/div/app-journal-syndicate-details/div/div[2]/div/div[3]/button').click();
+    console.log(await download.path());
+    await download.saveAs('Payroll_report.txt'); // Generar archivo de reporte de payroll
+
+    await page.waitForTimeout(1000);
+
+
 });
